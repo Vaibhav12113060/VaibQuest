b604bd12cf2be1c62ebbf6b4711ae64a065361dd
Vaibhav12113060 <vaibhavkum4@gmail.com>
2026-05-12 20:43:48 +0530

    feat(frontend): setup app entry, styling, and API configuration

---
+++ Frontend/src/api/axios.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+
+  withCredentials: true,
+});
+
+/*
+=====================================
+REQUEST INTERCEPTOR
+=====================================
+*/
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
